89b20f0cc9b30bad2f5fe50684565a7961b5f658
linpeijie-python <953137010@qq.com>
2020-10-22 19:38:30 +0800

    work3

---
+++ code3_calculator/calculatorCode/calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/22 19:21
+# @Author  : Mark
+# @File    : calculator.py
+# @Software: PyCharm
+# 被测试代码，里面有加减乘除的方法
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
---
+++ code3_calculator/calculatorTest/datas/test_calcu.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/22 19:29
+# @Author  : Mark
+# @File    : test_calcu.py
+# @Software: PyCharm
